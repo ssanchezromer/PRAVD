93568e1ccc0d79cfffe878af20dffec6b4d171cd
ssanchezromer <122234525+ssanchezromer@users.noreply.github.com>
2024-01-14 21:01:47 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def page_grafico_vehiculos():
     top_vehicles = ", ".join(top_vehicles)
 
     st.markdown(f"""
-    Los tipos de vehículos más implicados en accidentes son: **:red[{top_vehicles}]**.\n
+    Los tipos de vehículos más implicados en accidentes son: **:red[{str(top_vehicles)}]**.\n
     Realizamos un gráfico de barras y de tarta para ver la distribución de los 
     tipos de vehículos implicados en accidentes.\n  
     """)
